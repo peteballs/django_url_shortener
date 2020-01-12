a1803e29aff09bf1c964ba1f8976498413693c45
peteballs <gsinvestments.prh@gmail.com>
2020-01-12 05:47:23 -0600

    New

---
+++ url_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('', views.shorten_url, name = 'shorten'),
+    path('redirect/<slug:url_hash>', views.shortened_redirect, name = 'shortened_redirect'),
+]
+    
